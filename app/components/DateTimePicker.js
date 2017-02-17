50b7b8157271ae8f8da2b1f6a5c8025320a02c7b
Don Mamaril <mamarildon@gmail.com>
2017-02-16 17:45:56 -0800

    [feat] datetimepicker

--- app/components/DateTimePicker.js
+++ app/components/DateTimePicker.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React                from 'react';
+import { findDOMNode }      from 'react-dom';
+import { getAvailability }  from '../utils/firebaseHelpers.js';
+
+const DATEPICKER_CFGS = {
+    autoclose   : true,
+    minDate     : new Date(),
+    format      : 'mm-dd-yyyy'
+};
 
 class DateTimePicker extends React.Component {
 
@@ -6,40 +14,106 @@ class DateTimePicker extends React.Component {
 
         super(props);
 
-        this.state = {};
+        this.state              = { timeslots: [] };
+        this.getTimeSlots       = this.getTimeSlots.bind(this);
+        this.createTimeSlot     = this.createTimeSlot.bind(this);
+        this.handleChangeDate   = this.handleChangeDate.bind(this);
     }
 
-    /**
-     * [componentDidMount description]
-     *
-     * ONCHANGE IS NOT TRIGGERING
-     * 
-     * @return {[type]} [description]
-     */
+
     componentDidMount () {
-        $('#datepicker').datepicker();
-        $('#timepicker').timepicker();
+
+        let { calendar } = this.refs;
+
+        calendar = $(calendar).datepicker(DATEPICKER_CFGS);
+
+        calendar.on('clearDate', this.handleChangeDate)
+                .on('changeDate', this.handleChangeDate);
+
+        calendar = calendar.data('datepicker');
+
+        this.clearDates = calendar.clearDates.bind(calendar);
+        this.destroy    = calendar.destroy.bind(calendar);
+    }
+
+
+    componentWillUnmount() {
+        this.destroy();
+    }
+
+
+    getTimeSlots (date) {
+        this.setState({ selection: date });
+        return getAvailability(date);
+    }
+
+
+    clearDates () {
+        this.clearDates();
+    }
+
+    createTimeSlot (time={}, index) {
+
+        let { value, display_text } = time;
+
+        if (!display_text) {
+            return (<option value=""> --:-- </option> );
+        }
+
+        if (display_text === 'Fully Booked') {
+            this.state.is_full = true;
+        }
+
+        return (<option value={ value } key={ index }> { display_text } </option>);
+    }
+
+    async handleChangeDate(e) {
+
+        let { value } = e.target;
+
+        // reset time selections;
+        if (value.length === 0) {
+            return this.setState({ timeslots: [] });
+        }
+
+        // pull data from fb db;
+        let timeslots = await this.getTimeSlots(value);
+
+        debugger;
+
+        this.props.onChange(e);
+        this.setState({ timeslots });
     }
 
     render () {
-
         return (
             <div className="form-group">
                 <div className="form-headers">
                     <h2> When would you like us to come? </h2>
                 </div>
 
-                
-                <div className="col-md-6 col-sm-12 input-group date" id="datepicker">
-                    <input onChange={ this.props.onChange } type="text" className="form-control" />
-                    <span className="input-group-addon">
-                        <span className="glyphicon glyphicon-calendar"></span>
-                    </span>
+                <div className="col-md-6 col-sm-12 date" id="datepicker">
+                    <input ref="calendar" type="text" className="form-control" placeholder="MM-DD-YYYY"/>
                 </div>
 
-                <div className="col-md-6 col-sm-12 input-group bootstrap-timepicker timepicker">
-                    <input onChange={ this.props.onChange } id="timepicker" type="text" className="form-control input-small"/>
-                    <span className="input-group-addon"><i className="glyphicon glyphicon-time"></i></span>
+
+                <div className="col-md-6 col-sm-12">
+                    <select onChange={ this.props.onChange } required className="form-control">
+                        { 
+                            (!this.state.timeslots.length
+                                ? this.createTimeSlot()
+                                : this.state.timeslots.map(this.createTimeSlot))
+                        }
+                    </select>
+                </div>
+
+                <div className={ "col-md-12 text-center " + (this.state.is_full ? 'visible' : 'hidden') }>
+                    <h5>
+                        Due to high demand, there are no available times for { this.state.selection }.
+                    </h5>
+                    <h5>
+                        Please choose a different date.
+                    </h5>
                 </div>
             </div>
         );
--- app/utils/firebaseHelpers.js
+++ app/utils/firebaseHelpers.js
@@ -15,10 +15,20 @@ const pushRecord = (ref, val) => {
  * [setBooking description]
  * @param {[type]} info [description]
  */
-export async function setBooking (info) {
+export async function setBooking (booking) {
 
-    let key     = encodeKey(info.email);
+    let key     = encodeKey(booking.email);
     let ref     = firebase.database().ref(`booking/${key}`);
     
-    return await pushRecord(ref, info);
+    return await pushRecord(ref, booking);
+};
+
+export async function getAvailability (date) {
+
+    let ref                 = firebase.database().ref(`availabilitity`);
+    const NO_AVAILABILITY   = { value: null, display_text: 'Fully Booked' };
+
+    return new Promise((resolve) => {
+        ref.child(date).on('value', (ss) => resolve(ss.val() || [NO_AVAILABILITY]));
+    });
 };
